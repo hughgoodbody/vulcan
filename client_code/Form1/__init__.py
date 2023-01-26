08fde59a1363a8c2407c848851bfb305467eeb66
hugh@goodbodyeffects.com <hugh@goodbodyeffects.com>
2023-01-26 07:10:21 +0000

    Created onshape_api, onshape_api.client, onshape_api.onshape, onshape_api.utils and 1 more; Edited settings and Form1; Deleted user_config

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import stripe.checkout
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -16,6 +17,8 @@ class Form1(Form1Template):
 
     while not anvil.users.login_with_form(allow_remembered=True, remember_by_default=True, show_signup_option=True):
       pass
-    from .. import user_config  
-    print(user_config.currentUserConfig)
-    print(user_config.currentUserConfig['Users Suppliers'][0]['supplierName'])
+    #Get the current user data which will be required throughout the use of the app, saves repeated calls to tables
+    currentUserConfig = anvil.server.call('userConfig')
+    currentUser = currentUserConfig['User']
+    print(currentUserConfig)
+    print(currentUserConfig['Users Suppliers'][0]['supplierName'])
---
+++ server_code/user_config.py
@@ -0,0 +1,50 @@
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import anvil.secrets
+
+@anvil.server.callable
+def userConfig():
+  currentUserConfig = {
+   "User":None,
+   "Users Suppliers":[],
+   "Encoded Access Key":None,
+   "Encoded Secret Key":None,
+   "Order ID":None,
+   "Order Prefix":None,
+   "Order Reference":None
+}
+  currentUser = anvil.users.get_user()
+  if currentUser is not None:
+    currentUserConfig['User'] = currentUser
+    currentUserConfig['Encoded Access Key'] = currentUser['onshape_access_key_encoded']
+    currentUserConfig['Encoded Secret Key'] = currentUser['onshape_secret_key_encoded']
+    currentUserConfig['Order ID'] = app_tables.numbers.client_readable().get(owner=currentUser)['RefNumber']
+    currentUserConfig['Order Prefix'] = app_tables.numbers.client_readable().get(owner=currentUser)['RefPrefix']
+    currentUserConfig['Users Suppliers'] = app_tables.suppliers.client_readable().search(owner=[currentUser])
+    return currentUserConfig
+  else:
+    return None
+
+@anvil.server.callable
+def onshapeApiKeyCheck(user):  
+  if user['onshape_access_key_encrypted'] == None or user['onshape_secret_key_encrypted'] == None:
+    #print('TRUE')
+    return True
+  else:
+    #print('FALSE')
+    return False
+
+@anvil.server.callable
+def onshapeApiKeyRemove(user):
+  user.update(onshape_access_key_encrypted=None, onshape_secret_key_encrypted=None)
+  return
+
+@anvil.server.callable
+def onshapeApiKeyAdd(user, access, secret):
+  encryptedAccess = anvil.secrets.encrypt_with_key('vc_onshape_encryption_key',access)
+  encryptedSecret = anvil.secrets.encrypt_with_key('vc_onshape_encryption_key', secret)
+  user.update(onshape_access_key_encrypted=encryptedAccess, onshape_secret_key_encrypted=encryptedSecret)
+  return
