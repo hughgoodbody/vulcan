f29d7ebaa54512540fe19cc22e202dae868bf5c2
hugh@goodbodyeffects.com <hugh@goodbodyeffects.com>
2023-02-01 12:49:30 +0000

    Edited list_parts

--- server_code/list_parts.py
+++ server_code/list_parts.py
@@ -129,7 +129,7 @@ def list_parts_assembly(userData, documentInfo, configurationString, profileOpti
     #pprint(headerList)
   
   #Remove items excluded from laser Search
-  api_bom['rows'][:] = [x for x in api_bom['rows'] if x['headerIdToValue'].get(headerDict['Exclude From Laser Search']) == False]
+  #api_bom['rows'][:] = [x for x in api_bom['rows'] if x['headerIdToValue'].get(headerDict['Exclude From Laser Search']) == False]
   api_bom['rows'][:] = [x for x in api_bom['rows'] if x['headerIdToValue'].get(headerDict['Exclude from BOM']) == False]
   api_bom['rows'][:] = [x for x in api_bom['rows'] if x['itemSource'].get('isStandardContent') == False]
   #pprint(api_bom['rows'])
