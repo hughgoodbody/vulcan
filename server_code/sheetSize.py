613ede606c03707557b7a26f19e6cfc3d320ce2a
hugh@goodbodyeffects.com <hugh@goodbodyeffects.com>
2023-02-09 07:12:01 +0000

    Edited settings and sheetSize

--- server_code/sheetSize.py
+++ server_code/sheetSize.py
@@ -11,3 +11,27 @@ sheetSize = {'Page': 'A3',
              'Left Pad': 11,
              'Bottom Pad': 5,
 }
+
+def sheetParameters():
+  sheetSize = {'Page': 'A3',
+             'Width': 358,
+             'Height': 244,
+             'Border': 5,
+             'Horizontal Boxes': 3,
+             'Vertical Boxes': 3,
+             'Title Text': 6,
+             'Spacing': 3,
+             'Box Width': None,
+             'Box Height': None,
+             'Left Pad': 11,
+             'Bottom Pad': 5,
+}
+  
+sheetSize['Box Width'] = sheetSize['Width'] / sheetSize['Horizontal Boxes']
+sheetSize['Box Height'] = sheetSize['Height'] / sheetSize['Vertical Boxes']
+
+ImageStartPoint = (sheetSize['Border']+sheetSize['Spacing'], sheetSize['Border']+sheetSize['Bottom Pad']+(2*sheetSize['Title Text'])+(2*sheetSize['Spacing']))
+supplierStartPoint = (sheetSize['Border']+sheetSize['Spacing'] + sheetSize['Left Pad'], sheetSize['Border']+sheetSize['Bottom Pad']+(sheetSize['Spacing'])+(sheetSize['Title Text']))
+idStartPoint = (sheetSize['Border']+sheetSize['Spacing'] + sheetSize['Left Pad'], sheetSize['Border']+sheetSize['Bottom Pad'])
+refStartPoint = (sheetSize['Border']+sheetSize['Spacing'] + sheetSize['Left Pad'] + (sheetSize['Width']/2), sheetSize['Border']+sheetSize['Bottom Pad'])
+
