3d5604914fb4145ff569a3636609935b62347159
hugh@goodbodyeffects.com <hugh@goodbodyeffects.com>
2024-03-27 06:39:12 +0000

    Created NOTES

---
+++ client_code/NOTES/__init__.py
@@ -0,0 +1,11 @@
+Option to turn off DXF annotations (for hobby market)
+Save seperate thicknesses to thickness based folder (for hobby market)
+Save quantities of the same file, save 6 copies, if there are 6 (for hobby market)
+
+Geometry_test - change the round function to allow 1.5mm or 1.2mm, rather than rounding to nearest whole number
+
+Make split faces work, especially if coplanar
+
+Check composite parts and frames are working
+
+Check why Dave having problems when things like bearings and cylinders are included
