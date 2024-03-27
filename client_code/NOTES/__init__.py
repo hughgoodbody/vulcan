33f2bf1d7fdab5982e2a7340fd90f6d778447181
hugh@goodbodyeffects.com <hugh@goodbodyeffects.com>
2024-03-27 07:14:42 +0000

    Edited NOTES

--- client_code/NOTES/__init__.py
+++ client_code/NOTES/__init__.py
@@ -1,11 +1,17 @@
+
+ERRORS:
+Error when using custom BOM tables
+Additional qty on table entry not propogating to total quantity
+Geometry_test - change the round function to allow 1.5mm or 1.2mm, rather than rounding to nearest whole number
+Make split faces work, especially if coplanar
+
+
+FEATURES:
 Option to turn off DXF annotations (for hobby market)
 Save seperate thicknesses to thickness based folder (for hobby market)
 Save quantities of the same file, save 6 copies, if there are 6 (for hobby market)
 
-Geometry_test - change the round function to allow 1.5mm or 1.2mm, rather than rounding to nearest whole number
 
-Make split faces work, especially if coplanar
 
 Check composite parts and frames are working
-
 Check why Dave having problems when things like bearings and cylinders are included
